bc30b689dc82a5c8c1e268506fe04c6ac303e3d4
gowtham <gowthamp669@gmail.com>
2025-09-04 15:17:10 +0530

    6th commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
+
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+/*   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode> */
+
+  <HashRouter>
+  <App />
+</HashRouter>
+
 );
